f18393b15c08ae9890529b8ea96d339f1f345bb0
Catalin Moldovan <moldovancatalinst@gmail.com>
2018-05-09 15:10:25 +0300

    Buss location web socket communication

--- src/main/java/com/catalin/facultate/locationtracker/controller/BussLocationWebSocketHandler.java
+++ src/main/java/com/catalin/facultate/locationtracker/controller/BussLocationWebSocketHandler.java
@@ -1,19 +1,19 @@
 package com.catalin.facultate.locationtracker.controller;
 
+import com.catalin.facultate.locationtracker.dto.BussLocationDto;
 import com.catalin.facultate.locationtracker.model.Event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import java.time.Instant;
 
 @Slf4j
 @Controller
-public class LinesController {
+public class BussLocationWebSocketHandler {
 
     @MessageMapping("/new-coordinate")
     @SendTo("/topic/line")
@@ -23,22 +23,18 @@ public class LinesController {
                 .busLine(event.getBusLine())
                 .build();
 
-        log.info("Sending back {}", response);
+        log.info("no var Sending back {}", response);
 
         return response;
     }
 
     @MessageMapping("/new-coordinate/{line}")
     @SendTo("/topic/line")
-    public Event sendMessageVariable(@DestinationVariable("line") String line, @Payload Event event) {
-        Event response = Event.builder()
-                .timestamp(Instant.now())
-                .busLine(line)
-                .build();
+    public BussLocationDto sendMessageVariable(@DestinationVariable("line") String line,
+                                               @Payload BussLocationDto bussLocationDto) {
+        log.info("var Sending {} back {}", bussLocationDto, line);
 
-        log.info("Sending {} back {}", response, line);
-
-        return response;
+        return bussLocationDto;
     }
 
 }
